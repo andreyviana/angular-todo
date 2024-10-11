d44df2edcbcc7f529bd8b331ef37278de71e4f28
andreyviana <andreyviana125@gmail.com>
2024-10-11 13:36:50 -0300

    Estruturar e formatar o site

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LucideAngularModule, Trash, Pen } from 'lucide-angular'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LucideAngularModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'angular-todo';
+  
+  todoList = [
+    {todo: "Work", id: 1, isCompleted: true},
+    {todo: "Study", id: 2, isCompleted: true},
+    {todo: "Play video games", id: 3, isCompleted: false},
+  ]
+
+  readonly trash = Trash;
+  readonly pen = Pen;
 }
